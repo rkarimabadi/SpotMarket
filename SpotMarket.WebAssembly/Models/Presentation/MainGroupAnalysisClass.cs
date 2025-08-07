04cfacba0a34991bcbc5e067946b790b93000de5
rkarimabadi <rkarimabadi@gmail.com>
2025-08-07 18:10:58 +0330

    new generic widgets

---
+++ SpotMarket.WebAssembly/Models/Presentation/MainGroupAnalysisClass.cs
@@ -0,0 +1,17 @@
+﻿namespace SpotMarket.WebAssembly.Models.Presentation
+{
+    // --- مدل‌های داده برای این کامپوننت ---
+    public record CategorizedSubGroups(
+        List<SubGroupItem> OnOfferToday,
+        List<SubGroupItem> HottestDeals,
+        List<SubGroupItem> UpcomingOffers,
+        List<SubGroupItem> OtherGroups
+    );
+
+    public record SubGroupItem(
+        string Title,
+        string Subtitle,
+        string Stat,
+        string StatCssClass = ""
+    );
+}
