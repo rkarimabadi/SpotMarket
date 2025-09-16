de68dbbc345344309f7a4d1e13dbf2c96ab08802
reza karimabadi <rkarimabadi@gmail.com>
2025-09-17 00:06:51 +0330

    mobile project

---
+++ SpotMarket.MobileApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SpotMarket.MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SpotMarket.MobileApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SpotMarket.MobileApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
